6351430e54689d1bfd989d30e17bb1515606d505
Wilian Krinke <krinkewilian@gmail.com>
2023-05-03 03:30:57 -0300

    inserindo whitelist

--- api_teste_cors/app.js
+++ api_teste_cors/app.js
@@ -6,15 +6,21 @@ var getDatas = require('./datas/index.js')
 const app = express();
 const datas = getDatas();
 
-const corsOptionsGetDatas = {
-  origin: "http://localhost:3000",
-  optionsSuccessStatus: 200,
-  credentials: true
+var whitelist = ['http://localhost:3000', 'https://teste-chi-red.vercel.app']
+
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
 }
 
 app.use(bodyParser.json())
 
-app.get("/",cors(corsOptionsGetDatas),(req, res) => {
+app.get("/",cors(corsOptions),(req, res) => {
   res.send(datas)
 })
 
